5cb6176aa999cf7fa5d3a00cbec0c03beb9b9d1d
kipler8654 <adfd130@gmail.com>
2023-06-10 20:18:30 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        Animal animal = new Cat();
+        Animal animal1 = new Dog();
+        animal.say();
+        animal1.say();
+        try{
+            Cat cat = (Cat) animal1;
+        }catch(ClassCastException e){
+            System.out.println("Error");
+        }
     }
 }
