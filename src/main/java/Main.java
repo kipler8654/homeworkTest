02f554a8d573c13a9d719dfd45a5629163ea95df
kipler8654 <adfd130@gmail.com>
2023-06-18 19:53:21 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,7 +11,7 @@ public class Main {
             System.out.println("Error");
         }
     }
-    public static void animalSay(Animal animal){
+    public static void animalSay(Animal animal) {
         animal.say();
     }
 }
