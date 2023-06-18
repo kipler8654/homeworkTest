9eec9d9258d8912e3488e02c47adddd61da18385
kipler8654 <adfd130@gmail.com>
2023-06-18 19:55:04 +0300

    first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
-        Cat cat = new Cat();
-        Dog dog = new Dog();
-        Animal animal = new Dog();
-        animalSay(cat);
-        animalSay(dog);
-        try {
-            Cat cat1 = (Cat) animal;
-        } catch (ClassCastException e) {
-            System.out.println("Error");
+    public class Main
+    {
+        public static void main(String[] args) {
+            Cat cat = new Cat();
+            Dog dog = new Dog();
+            Animal animal = new Dog();
+            animalSay(cat);
+            animalSay(dog);
+            try {
+                Cat cat1 = (Cat) animal;
+            } catch (ClassCastException e) {
+                System.out.println("Error");
+            }
+        }
+        public static void animalSay(Animal animal) {
+            animal.say();
         }
     }
-    public static void animalSay(Animal animal) {
-        animal.say();
-    }
-}
